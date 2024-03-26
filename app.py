c75326cd36f57ecd92dba4b244b9c991711741e0
lucaslamm <97729414+lucasenetjarn@users.noreply.github.com>
2024-03-26 12:05:27 +0100

    dsföoiygkyldjsupoåi

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask import render_template
+from database import get_all_movies
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    
-    return render_template('index.html')
+    data = get_all_movies()
+    return render_template('index.html', data=data)
 
 
 if __name__ == '__main__':
